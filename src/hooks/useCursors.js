ffe58978e21b7a5fec83bb6c5dca4cfce592d4cb
miri <56790941+miri-san-so@users.noreply.github.com>
2026-05-09 13:42:50 +0530

    Update useCursors.js

--- src/hooks/useCursors.js
+++ src/hooks/useCursors.js
@@ -1,7 +1,7 @@
 import { useState, useEffect, useCallback, useRef } from "react";
 import logger from "../utils/logger";
 
-const WS_URL = process.env.REACT_APP_WS_URL || "ws://getsnapdrop.in/react-editor";
+const WS_URL = process.env.REACT_APP_WS_URL || "wss://getsnapdrop.in/react-editor";
 const THROTTLE_MS = 50;
 
 /**
